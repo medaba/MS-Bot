5c5acd6ba98c779176584a7a6473cbb4a47e80d5
medaba <freedaba@protonmail.com>
2021-02-18 16:24:01 +0700

    new structure

--- utils/misc/logger.py
+++ utils/misc/logger.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    filename='logs/logger.log',
+    filename='logger.log',
     format='%(asctime)s | %(name)s | %(levelname)-4s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
     )
